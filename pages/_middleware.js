f5424a18282e7a7fe4a31ac9f594a26ac028ff09
butthx <tanya.duwi23@gmail.com>
2021-12-12 20:56:44 +0700

    remove _middleware

--- pages/_middleware.js
+++
@@ -1,9 +0,0 @@
-import {NextRequest,NextResponse} from "next/server"
-
-export default function middleware (req) {
-  let userAgent = req.headers.get('user-agent');
-  console.log(userAgent);
-  if(userAgent.toLowerCase().trim() == "telegrambot"){
-    return NextResponse.redirect(encodeURI(`https://t.me/iv?url=https://tgsnake.js.org${req.url.pathname}&rhash=ce6600454132da`))
-  }
-}
