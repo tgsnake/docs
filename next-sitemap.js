2361022ff14ed0b2b7d2ca362d31e5652b0bfdb5
butthx <68604099+butthx@users.noreply.github.com>
2022-04-14 11:25:05 +0700

    add sitemap url condition production deployment.

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "https://tgsnake.js.org",
+  siteUrl: (process.env.VERCEL_URL && process.env.VERCEL_ENV && process.env.VERCEL_ENV !== "production") ? `https://${process.env.VERCEL_URL}` : "https://tgsnake.js.org",
   generateRobotsTxt: true,
   sitemapSize: 7000,
   exclude : ["/login","/api","/pages/*","/beta/*"]
