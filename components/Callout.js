5767efc4a9bc1dede13fbd436d78b7ec98458a98
butthx <68604099+butthx@users.noreply.github.com>
2022-04-15 09:42:54 +0700

    fixed rss url when production deployment add telegram.download docs

--- components/Callout.js
+++ components/Callout.js
@@ -10,6 +10,7 @@ const themes = {
   error: 'bg-red-100 border border-red-200 text-red-900 dark:text-red-200 dark:bg-red-900 dark:bg-opacity-30 dark:border-opacity-30',
   warning: 'bg-yellow-50 border border-yellow-100 text-yellow-900 dark:text-yellow-200 dark:bg-yellow-700 dark:bg-opacity-30',
   success: 'bg-green-50 border border-green-100 text-green-900 dark:text-green-200 dark:bg-green-700 dark:bg-opacity-30',
+  green: 'bg-green-50 border border-green-100 text-green-900 dark:text-green-200 dark:bg-green-700 dark:bg-opacity-30',
   blue: 'bg-blue-50 border border-blue-100 text-blue-900 dark:text-blue-200 dark:bg-blue-700 dark:bg-opacity-30',
   sky: 'bg-sky-50 border border-sky-100 text-sky-900 dark:text-sky-200 dark:bg-sky-700 dark:bg-opacity-30',
 };
--- pages/_middleware.js
+++ pages/_middleware.js
@@ -4,21 +4,22 @@ let regex = /^\/?((pages|api)|((.*\/)?images))/i;
 export async function middleware (req){
   let userAgent = req.headers.get("user-agent");
   if(userAgent == "TelegramBot (like TwitterBot)" && (!regex.test(req.nextUrl.pathname))){
-    let locale = req.nextUrl.locale == 'default' ? 'en' : req.nextUrl.locale
-    console.log(locale)
-    let home = ["/",locale,`/${locale}`,`/${locale}/`]
-    let paths = path.join("/pages",home.includes(req.nextUrl.pathname) ? `${locale}/index.html` : `/${locale}/${req.nextUrl.pathname}.html`)
-    let res = await fetch(`https://${process.env.VERCEL_URL}/api/content.json`)
-    let json = await res.json() || []
-    console.log(`${userAgent} - ${paths}`)
+    let locale = req.nextUrl.locale == 'default' ? 'en' : req.nextUrl.locale;
+    console.log(locale);
+    let home = ["/",locale,`/${locale}`,`/${locale}/`];
+    let paths = path.join("/pages",home.includes(req.nextUrl.pathname) ? `${locale}/index.html` : `/${locale}/${req.nextUrl.pathname}.html`);
+    let url = (process.env.VERCEL_URL && process.env.VERCEL_ENV && process.env.VERCEL_ENV !== "production") ? `https://${process.env.VERCEL_URL}` : "https://tgsnake.js.org";
+    let res = await fetch(`${url}/api/content.json`);
+    let json = await res.json() || [];
+    console.log(`${userAgent} - ${paths}`);
     if(json.includes(paths.replace(/\.html$/,""))){
-      return NextResponse.rewrite(paths)
+      return NextResponse.rewrite(paths);
     }
     if(json.includes(paths.replace(`/pages/${locale}`,`/pages/en`).replace(/\.html$/,""))){
-      console.log(`Redirecting to ${paths.replace(`/pages/${locale}`,`/pages/en`)}`)
-      return NextResponse.rewrite(paths.replace(`/pages/${locale}`,`/pages/en`))
+      console.log(`Redirecting to ${paths.replace(`/pages/${locale}`,`/pages/en`)}`);
+      return NextResponse.rewrite(paths.replace(`/pages/${locale}`,`/pages/en`));
     }
-    return NextResponse.rewrite(`/pages/unsupported.html`)
+    return NextResponse.rewrite(`/pages/unsupported.html`);
   }
   return;
 }
